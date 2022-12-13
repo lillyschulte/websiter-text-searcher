1d8bf6bb25270efc31da5df4d15e6c2e3ea35cfc
lillyschulte <rickschulte@hotmail.nl>
2022-12-13 13:20:43 +0100

    Added reccusive search

--- main.py
+++ main.py
@@ -1,11 +1,6 @@
-import requests
-
-def search_keyword():
-  # Ask the user for the URL to search
-  url = input("Enter the URL to search: ")
-
-  # Ask the user for the keyword to search for
-  keyword = input("Enter the keyword to search for: ")
+def search_keyword(url, keyword):
+  import requests
+  import re
 
   # Use the requests module to fetch the HTML of the website
   response = requests.get(url)
@@ -15,9 +10,14 @@ def search_keyword():
     # Get the HTML of the website as a string
     html = response.text
 
+    # Use a regular expression to find all URLs in the HTML
+    url_regex = r'https?://\S+'
+    urls = re.findall(url_regex, html)
+
     # Search for the keyword in the HTML
     occurrences = []
     index = 0
+    line_number = 1
     while True:
       # Find the next occurrence of the keyword in the HTML
       index = html.find(keyword, index)
@@ -25,17 +25,38 @@ def search_keyword():
         # Keyword not found, exit the loop
         break
       else:
-        # Keyword found, add the index to the list of occurrences
-        occurrences.append(index)
+        # Keyword found, add the line number and index to the list of occurrences
+        occurrences.append((line_number, index))
+
+        # Increment the line number if a newline character is found
+        if html[index] == "\n":
+          line_number += 1
 
         # Increment the index to search for the next occurrence
         index += len(keyword)
+
+    # Recursively search the HTML of any underlying web pages
+    visited_urls = []
+    for url in urls:
+      # Skip URLs that have already been visited
+      if url in visited_urls:
+        continue
+
+      # Add the URL to the list of visited URLs
+      visited_urls.append(url)
+
+      # Search the HTML of the underlying web page
+      subpage_occurrences = search_keyword(url, keyword)
+
+      # Add the occurrences found in the underlying web page to the list of occurrences
+      occurrences.extend(subpage_occurrences)
 
     # Return the list of occurrences
     return occurrences
   else:
     print(f"Failed to fetch HTML for {url}")
 
-# Test the search_keyword function
-occurrences = search_keyword()
-print(occurrences)
+url = input("URL:")
+keyword = input("Keyword:")
+search_keyword(url, keyword)
+
